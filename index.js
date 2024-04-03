8630f62afa69a2943030581e1f1afd98854ce88a
hellen kimani <astridlee80@gmail.com>
2024-04-03 22:51:51 +1000

    Lab done

---
+++ index.js
@@ -0,0 +1,19 @@
+let result = '';
+const divide  = () => {
+    let a = 2000;
+    let b = 100;
+    result = a/b;
+    return result;
+} 
+const square = (x) => {
+    x = x * x;
+    return x;
+}
+square(2);
+
+let sum = '';
+const add = (a,b) => {
+    sum = a + b;
+    return sum;
+}
+add(3,4);
